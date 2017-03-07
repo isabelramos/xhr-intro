8d0ccf79d49d2b995ba7a37989688b39fdfd58d6
Isabel Ramos <iramos1226@gmail.com>
2017-03-06 21:19:22 -0600

    completed exercise

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+var dinoContainer = document.getElementById("dino-container");
+
+
+
+function makeDom (xhrData) {
+	var dinosaurString = "";
+	var currentDinosaur;
+	for (var i=0; i<xhrData.dinosaurs.length; i++) {
+		currentDinosaur = xhrData.dinosaurs[i];
+
+		dinosaurString += `<div class="col-sm-6 col-md-4">`;
+		dinosaurString += `<div class="thumbnail">`;
+		dinosaurString += `<img src="${currentDinosaur.url}" alt="dinosaur">`;
+		dinosaurString += `<div class="caption">`;
+		dinosaurString += `<h3>${currentDinosaur.name}</h3>`;
+		dinosaurString += `<p>${currentDinosaur.type}</p>`;
+		dinosaurString += `<p> Likes to eat: ${currentDinosaur.food}</p>`;		
+		dinosaurString += `</div></div></div>`;
+	}
+
+	dinoContainer.innerHTML = dinosaurString;
+}
+
+
 function executeThisCodeAfterFileLoad () {
-	console.log("woo!");
+	var data = JSON.parse(this.responseText);
+	console.log("woo!", data);
+	makeDom(data);
 }
 
 function executeThisCodeAfterFileFail () {
@@ -12,4 +38,3 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoad);
 myRequest.addEventListener("error", executeThisCodeAfterFileFail);
 myRequest.open("GET", "dinosaurs.json");
 myRequest.send();
-console.log(myRequest);
