d00afc3c9f858b8ebba1c6531d4809fe49e7822a
Isabel Ramos <iramos1226@gmail.com>
2017-03-06 20:01:52 -0600

    added json file

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("hello!");
+function executeThisCodeAfterFileLoad () {
+	console.log("woo!");
+}
+
+function executeThisCodeAfterFileFail () {
+	console.log("failureeee!");
+}
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoad);
+myRequest.addEventListener("error", executeThisCodeAfterFileFail);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log(myRequest);
